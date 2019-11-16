c964c9ec67f17746f2fb42a8b1251db7eb694f46
Juan Cajamarca <juancajamarca@outlook.es>
2019-11-15 21:44:09 -0500

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,80 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
+import io from 'socket.io-client'
+import { Form, FormGroup, Label, Input } from 'reactstrap'
+import Swal from 'sweetalert2'
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+export default class App extends Component {
+  constructor() {
+    super()
+    this.state = {
+      socket: io('localhost:4200'),
+      response: false
+    }
+  }
 
-export default App;
+  componentDidMount() {
+    this.state.socket.on('connect', () => {
+      console.log('Client connected')
+    })
+    
+    this.state.socket.on('disconnect', () => {
+      Swal.fire({
+        icon: 'error',
+        title: 'Oops...!!!',
+        text: 'You are offline!'
+      })
+    })
+
+    this.state.socket.on('welcomeMessage', (message, callback) => {
+      callback()
+      Swal.fire(
+        message.title ? message.title : '',
+        message.message ? message.message : '',
+        'success'
+      )
+    })
+  }
+
+  sendMessage(evt) {
+    evt.preventDefault()
+    const formData = new window.FormData(evt.target)
+    const username = formData.get('username')
+    const message = formData.get('message')
+    
+    this.state.socket.emit('sendMessage', {
+      user: username,
+      message
+    }, function() {
+      console.log('Message received by Server!')
+    })
+
+    Swal.fire({
+      position: 'top-end',
+      icon: 'success',
+      title: 'Message sent!',
+      showConfirmButton: false,
+      timer: 1500
+    })
+  }
+
+  render() {
+    return (
+      <div className="App" style={{ width: '30%', marginLeft: '35%', marginTop: '3em' }}>
+        <Form onSubmit={this.sendMessage.bind(this)}>
+          <FormGroup>
+            <Label for="username">Username:</Label>
+            <Input id="username" type="text" name="username" />
+          </FormGroup>
+          <FormGroup>
+            <Label for="message">Message:</Label>
+            <Input id="message" type="text" name="message" />
+          </FormGroup>
+          <FormGroup>
+            <Input type="submit" value="Send message" className="btn btn-primary" />
+          </FormGroup>
+        </Form>
+      </div>
+    );
+  }
+}
